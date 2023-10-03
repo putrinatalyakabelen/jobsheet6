b0e23c70f30d26f3698b78a60a5264abcc3cc372
EkaPutri <144087250+putrinatalyakabelen@users.noreply.github.com>
2023-10-03 17:01:31 +0700

    Add files via upload

---
+++ Latihan1_11.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Latihan1_11 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bil1 =28;
+        int bil2 =54;
+        int bil3 =15;
+if (bil1 >= bil2) {
+            if (bil1 >= bil3) {
+                System.out.println("Bilangan terbesar: " + bil1);
+            } else {
+                System.out.println("Bilangan terbesar: " + bil3);
+            }
+        } else {
+            if (bil2 >= bil3) {
+                System.out.println("Bilangan terbesar: " + bil2);
+            } else {
+                System.out.println("Bilangan terbesar: " + bil3);
+    }
+}
+}
+}
